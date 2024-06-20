20cedc66e3b46827c40b111f32c44c3ad5861fd4
RikiRilis <thewhitzip@gmail.com>
2024-06-19 23:21:55 -0400

    App Done

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Search:</h5>
+    <input type="text" class="form-control" placeholder="Search gifs..." (keyup.enter)="searchTag()" #txtTagInput>
+  `
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) { }
+
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
+}
